4d08dff18a976fd34d9fa8c1a8e0e5c2eaf894f1
qcodelabsllc <84394923+quabynah-bilson@users.noreply.github.com>
2023-07-09 06:38:09 +0000

    updated endpoint url

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 import Image from "next/image";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { AdminAuthenticationServiceClient } from "@/generated/Admin_serviceServiceClientPb";
 import { Empty } from "@/generated/common_pb";
 
 export default function Home() {
-  // let url = "http://0.0.0.0:8080";
-  let url = "https://afs-admin-proxy.fly.dev:443";
+  const [token, setToken] = useState("");
+
+  let url = "http://0.0.0.0:8999";
+  // let url = "https://afs-admin-proxy.fly.dev:443";
 
   // get the public access token
   const getPublicToken = async () => {
@@ -16,7 +18,7 @@ export default function Home() {
       if (err) {
         console.log(err);
       } else {
-        alert(response.getToken());
+        setToken(response.getToken());
       }
     });
   };
@@ -52,7 +54,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
+      <div className="relative flex flex-col place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
           src="/next.svg"
@@ -61,6 +63,9 @@ export default function Home() {
           height={37}
           priority
         />
+        <p className="max-w-sm text-sm mt-5">
+          {token ? token : "loading token..."}
+        </p>
       </div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
